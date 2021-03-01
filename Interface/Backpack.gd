0ae63238d60de8e42a46cec298e9973ab9150316
kleimaj <jacobakleiman@gmail.com>
2021-02-28 18:00:24 -0800

    Adds Member variables to Backpack.gd

--- Interface/Backpack.gd
+++ Interface/Backpack.gd
@@ -1,2 +1,18 @@
 extends Control
 
+# The Pill Texture Rect
+var selected_pill = null
+# The Selected Projectile Index (0, 1, or 2)
+var selected_idx = 0
+onready var grid = $Background/HBoxContainer/ItemGrid
+
+# Maps the Projectiles to its indices
+var ProjectileMap = {}
+
+var ItemText = {
+	"Blue Pill": "Blue pills are best against the blue type of mushrooms!",
+	"Red Pill": "Red pills are best against the red type of mushrooms!",
+	"Green Pill": "Green pills are best against the green type of mushrooms!",
+}
+
+
