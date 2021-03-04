03d646e5dfa4ddfed35efc96e46f79d17b0ec8ab
ryandrewd <ryan@nextretail.com>
2021-03-04 13:51:16 -0500

    Control changes

--- Shooting/Scripts/Tank.gd
+++ Shooting/Scripts/Tank.gd
@@ -11,15 +11,23 @@ func _ready():
 	$ShootTimer.wait_time = gun_cooldown
 
 func control(delta):
-	$Muzzle.look_at(get_global_mouse_position())
+
+func control(delta):
+
+velocity = Vector2()
+velocity = Vector2((get_viewport().get_mouse_position() - position).x,0)
+#	if Input.is_action_pressed('forward'):
+#		velocity = Vector2(speed, 0)
+#	if Input.is_action_pressed('back'):
+#		velocity = Vector2(-speed, 0)
+if Input.is_action_just_pressed('click') || changemuzzle == true:
 	velocity = Vector2()
-	if Input.is_action_pressed('forward'):
-		velocity = Vector2(speed, 0)
-	if Input.is_action_pressed('back'):
-		velocity = Vector2(-speed, 0)
-	if Input.is_action_just_pressed('click'):
-		shoot()
-		
+	changemuzzle = true
+	$Muzzle.look_at(get_global_mouse_position())
+if Input.is_action_just_released("click"):
+	changemuzzle = false
+	shoot()
+
 func shoot():
 		$ShootTimer.start()
 		var b = Bullet.instance()
@@ -27,7 +35,7 @@ func shoot():
 		b.transform = $Muzzle/Position2D.global_transform
 		b.velocity = b.transform.x * 800
 		b.gravity = 250
-		
+
 func _physics_process(delta):
 	control(delta)
 	move_and_slide(velocity)
