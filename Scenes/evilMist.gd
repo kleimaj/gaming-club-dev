3d11aa224746c7113e942b7d0ede45242b058949
kleimaj <jacobakleiman@gmail.com>
2021-03-23 14:13:08 -0700

    adds moveUp and moveDown functions to fog

--- Scenes/evilMist.gd
+++ Scenes/evilMist.gd
@@ -7,3 +7,9 @@ func _on_Mushroom2_body_shape_entered(body_id, body, body_shape, area_shape):
 
 func _on_Mushroom_body_shape_entered(body_id, body, body_shape, area_shape):
 	position.y += 25
+
+func moveUp():
+	position.y -= 25
+
+func moveDown():
+	position.y += 25
