74caf7db8ec9070130f25d7fa81674cf4a3e4b47
ryandrewd <ryan@nextretail.com>
2021-03-23 15:07:51 -0400

    mist movement

---
+++ Scenes/evilMist.gd
@@ -0,0 +1,9 @@
+extends Sprite
+
+
+func _on_Mushroom2_body_shape_entered(body_id, body, body_shape, area_shape):
+	position.y -= 25
+
+
+func _on_Mushroom_body_shape_entered(body_id, body, body_shape, area_shape):
+	position.y += 25
