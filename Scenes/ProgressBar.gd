982318519e1a59499f3831402d7ccac4ac102332
kleimaj <jacobakleiman@gmail.com>
2021-03-23 14:07:17 -0700

    adds increment and decrement value functions to progress bar

--- Scenes/ProgressBar.gd
+++ Scenes/ProgressBar.gd
@@ -8,3 +8,8 @@ func _on_Mushroom2_body_shape_entered(body_id, body, body_shape, area_shape):
 func _on_Mushroom_body_shape_entered(body_id, body, body_shape, area_shape):
 	value -= 25
 
+func incrementValue():
+	value += 25
+
+func decrementValue():
+	value -= 25
