c748be61620a587c9eaa524f94f9a5dd8ae7d05d
kleimaj <jacobakleiman@gmail.com>
2021-03-25 00:05:17 -0700

    adds PageMap and IconButtons to Book

--- Scripts/Book.gd
+++ Scripts/Book.gd
@@ -6,9 +6,36 @@ var tween
 
 export var speed = 850
 
+var PageMap = [
+	{
+		"mushroom_title": "[center]Blue Fungi[/center]",
+		"mushroom_sprite": "res://Assets/GFX/UI/blueMushIcon.png",
+		"juice_title": "[center]Treated by Pink Juice[/center]",
+		"juice_sprite": "res://Assets/GFX/UI/newBottles/bottle2.png"
+	},
+	{
+		"mushroom_title": "[center]Green Fungi[/center]",
+		"mushroom_sprite":"res://Assets/GFX/UI/greenMushIcon.png" ,
+		"juice_title": "[center]Treated by Pink Juice[/center]",
+		"juice_sprite": "res://Assets/GFX/UI/newBottles/bottle2.png"
+	},
+	{
+		"mushroom_title": "[center]Blue Spotted Fungi[/center]",
+		"mushroom_sprite": "res://Assets/GFX/UI/blueSpotsMushIcon.png",
+		"juice_title": "[center]Treated by Yellow Juice[/center]",
+		"juice_sprite": "res://Assets/GFX/UI/newBottles/bottle1.png"
+	},
+	{
+		"mushroom_title": "[center]Green Spotted Fungi[/center]",
+		"mushroom_sprite": "res://Assets/GFX/UI/greenSpotsMushIcon.png",
+		"juice_title": "[center]Treated by Yellow Juice[/center]",
+		"juice_sprite": "res://Assets/GFX/UI/newBottles/bottle1.png"
+	} 
+]
+
 func _ready():
 	set_process(true)
-#	show_book_description()
+	$BookTexture/HBoxContainer/RightContainer/AnimationPlayer.play("ShowDescription")
 
 func _process(delta):
 #	follow.set_offset(follow.get_offset() + speed * delta)
@@ -23,3 +50,11 @@ func animatePencil():
 #	description.percent_visible = 0
 #	$BookTexture/WritingTween.interpolate_property($BookTexture/HBoxContainer/RightContainer/Description, "percent_visible", 0, 1, 6, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 #	$BookTexture/WritingTween.start()
+
+
+func _on_LeftButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_RightButton_pressed():
+	pass # Replace with function body.
