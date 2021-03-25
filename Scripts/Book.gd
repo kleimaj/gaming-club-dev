29e5e88d52f32405ff5d3b03e008931866138e05
kleimaj <jacobakleiman@gmail.com>
2021-03-25 00:25:06 -0700

    turns pages with icon buttons and switches page contents

--- Scripts/Book.gd
+++ Scripts/Book.gd
@@ -4,8 +4,7 @@ onready var follow = $BookTexture/Path2D/PathFollow2D
 onready var description = $BookTexture/HBoxContainer/RightContainer/Description
 var tween
 
-export var speed = 850
-
+var page_idx = 0
 var PageMap = [
 	{
 		"mushroom_title": "[center]Blue Fungi[/center]",
@@ -36,6 +35,7 @@ var PageMap = [
 func _ready():
 	set_process(true)
 	$BookTexture/HBoxContainer/RightContainer/AnimationPlayer.play("ShowDescription")
+	showPage(page_idx)
 
 func _process(delta):
 #	follow.set_offset(follow.get_offset() + speed * delta)
@@ -46,6 +46,13 @@ func animatePencil():
 	add_child(tween)
 	tween.interpolate_property(follow, "unit_offset", 0, 1, 6, tween.TRANS_LINEAR, tween.EASE_IN_OUT)
 
+func showPage(idx):
+	var pageContents = PageMap[idx]
+	$BookTexture/HBoxContainer/LeftContainer/MushroomTitle.bbcode_text = pageContents["mushroom_title"]
+	$BookTexture/HBoxContainer/LeftContainer/MushroomContainer/Mushroom.texture = load(pageContents["mushroom_sprite"])
+	$BookTexture/HBoxContainer/LeftContainer/JuiceTitle.bbcode_text = pageContents["juice_title"]
+	$BookTexture/HBoxContainer/LeftContainer/JuiceContainer/Juice.texture = load(pageContents["juice_sprite"])
+
 #func show_book_description():
 #	description.percent_visible = 0
 #	$BookTexture/WritingTween.interpolate_property($BookTexture/HBoxContainer/RightContainer/Description, "percent_visible", 0, 1, 6, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
@@ -53,8 +60,14 @@ func animatePencil():
 
 
 func _on_LeftButton_pressed():
-	pass # Replace with function body.
+	page_idx -=1
+	if page_idx == -1:
+		page_idx = 3
+	showPage(page_idx)
 
 
 func _on_RightButton_pressed():
-	pass # Replace with function body.
+	page_idx +=1
+	if page_idx == 4:
+		page_idx = 0
+	showPage(page_idx)
