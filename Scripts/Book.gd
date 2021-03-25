0f3e385917d9bcecebe25a9899b81c1d1a99f433
kleimaj <jacobakleiman@gmail.com>
2021-03-24 23:22:57 -0700

    animates pencil to match bbcode percent visible

--- Scripts/Book.gd
+++ Scripts/Book.gd
@@ -2,15 +2,24 @@ extends Control
 
 onready var follow = $BookTexture/Path2D/PathFollow2D
 onready var description = $BookTexture/HBoxContainer/RightContainer/Description
+var tween
 
-export var speed = 750
+export var speed = 850
 
 func _ready():
 	set_process(true)
-	show_book_description()
+#	show_book_description()
 
 func _process(delta):
-	follow.set_offset(follow.get_offset() + speed * delta)
-
-func show_book_description():
+#	follow.set_offset(follow.get_offset() + speed * delta)
 	pass
+
+func animatePencil():
+	tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(follow, "unit_offset", 0, 1, 6, tween.TRANS_LINEAR, tween.EASE_IN_OUT)
+
+#func show_book_description():
+#	description.percent_visible = 0
+#	$BookTexture/WritingTween.interpolate_property($BookTexture/HBoxContainer/RightContainer/Description, "percent_visible", 0, 1, 6, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+#	$BookTexture/WritingTween.start()
