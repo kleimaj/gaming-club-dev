19d8cbb633796926e011ee5b6374e4f722742be1
kleimaj <jacobakleiman@gmail.com>
2021-03-26 11:26:53 -0700

    adds Skipping key shortcuts to Cutscene.gd

--- Scripts/Cutscene.gd
+++ Scripts/Cutscene.gd
@@ -1,2 +1,9 @@
 extends Control
 
+func _unhandled_key_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_S:
+			# Skip to next scene
+			pass
+		elif event.pressed and event.scancode == KEY_G:
+			get_tree().change_scene("res://Scenes/Game.tscn")
