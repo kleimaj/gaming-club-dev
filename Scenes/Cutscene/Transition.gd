0cae28ae761634df2a584ff7931ce0c71edf4267
kleimaj <jacobakleiman@gmail.com>
2021-03-30 10:44:16 -0700

    changes Book to use new font and assets

--- Scenes/Cutscene/Transition.gd
+++ Scenes/Cutscene/Transition.gd
@@ -97,36 +97,42 @@ var ItemMap = {
 	"BlueMushroom": {
 		"name": "[center]Blue Mushroom[/center]",
 		"sprite": load("res://Assets/GFX/UI/blueMushIcon.png"),
+		"color": "#048B99",
 		"description": "The blue mushroom is the most common in this terrarium. Approximately, 85-95% of a healthy terrarium includes them. [b]The mushroom lives in sunlight,[/b] but thrives in darkness, [b]1 dose of the pink spray is needed to treat it when it's dark.[/b]",
 		"notes": "The blue mushroom is similar to Candida albicans. Approximately, 85-95% of yeast cultured in a healthy vagina is Candida albicans. An overgrowth of this common yeast caused by an imbalance in the vagina can result in an infection. Fungistatic drugs which can be topical or oral are commonly used to treat these infections. "
 	},
 	"BlueSpottedMushroom": {
 		"name": "[center]Blue Spotted Mushroom[/center]",
 		"sprite": load("res://Assets/GFX/UI/blueSpotsMushIcon.png"),
+		"color": "#048B99",
 		"description": "The blue mushroom with spots is very similar to the blue mushroom without spots. [b]The mushroom lives in sunlight,[/b] but is more difficult to treat than the one without spots when it gets out of control which can occur in darkness. When it thrives in darkness, [b]1 dose of the yellow spray is needed to treat it.[/b] ",
 		"notes": "The blue mushroom with spots is similar to Candida albicans. Approximately, 85-95% of yeast cultured in a healthy vagina is Candida albicans. An overgrowth of this common yeast caused by an imbalance in the vagina can result in an infection. Although certain fungistatic drugs which can be topical or oral may be used, resistance to certain drugs can occur. Other treatment options need to be considered."
 	},
 	"GreenMushroom": {
 		"name": "[center]Green Mushroom[/center]",
 		"sprite": load("res://Assets/GFX/UI/greenMushIcon.png"),
+		"color": "#0E8E00",
 		"description": "The green mushroom is the least common fungi in this terrarium. Rarely is it seen in a healthy terrarium. [b]The mushroom thrives in dark environments,[/b] but if it thrives, [b]2 doses of the pink spray is needed to treat it.[/b]",
 		"notes": "The green mushroom is similar to Candida non-albicans. It is rarely present in a healthy vagina. An overgrowth of this less common yeast caused by an imbalance in the vagina can result in an infection. Fungistatic drugs which can be topical or oral are commonly used to treat these infections but may need a longer duration  of treatment to eradicate the signs and symptoms."
 	},
 	"GreenSpottedMushroom": {
 		"name": "[center]Green Spotted Mushroom[/center]",
 		"sprite": load("res://Assets/GFX/UI/greenSpotsMushIcon.png"),
+		"color": "#0E8E00",
 		"description": "The green mushroom with spots is the least common fungi in this terrarium. Rarely is it seen in a healthy terrarium. [b]The mushroom thrives in dark environments, but is more difficult to treat than the one without spots when it gets out of control.[/b] If it thrives too much, [b]2 doses of the yellow spray is needed to treat it.[/b]",
 		"notes": "The green mushroom with spots is similar to Candida non-albicans. It is rarely present in a healthy vagina. An overgrowth of this less common yeast caused by an imbalance in the vagina can result in an infection. Although certain fungistatic drugs which can be topical or oral may be used, resistance to certain drugs can occur. Other treatment options become very limited but need to be considered."
 	},
 	"YellowSpray": {
 		"name": "[center]Yellow Spray[/center]",
 		"sprite": load("res://Assets/GFX/UI/newBottles/bottle1.png"),
+		"color": "#000",
 		"description": "The yellow spray is used on the overgrowth of blue and green mushrooms with  spots to trigger the the stopping of spores to fend off the dark mist. Sometimes certain mushrooms need more spray for a longer duration of time to be effective.",
 		"notes": "The yellow spray is similar to various fungistatic drugs which can be topical or oral. Patients may develop a resistance to this drug resulting in the need for longer duration of treatment"
 	},
 	"PinkSpray": {
 		"name": "[center]Pink Spray[/center]",
 		"sprite": load("res://Assets/GFX/UI/newBottles/bottle2.png"),
+		"color": "#000",
 		"description": "The pink spray is used on the overgrowth of blue and green mushrooms to trigger the stopping of spores to fend off the dark mist. Sometimes certain mushrooms need more spray for a longer duration of time to be effective.",
 		"notes": "The pink spray is similar to various fungistatic drugs which can be topical or oral. Patients may develop a resistance to this drug resulting in the need for longer duration of treatment"
 	}
@@ -160,10 +166,29 @@ func dialog_finished():
 		$CanvasLayer/BookButton.hide()
 		$AnimationPlayer.play("EndScene")
 		
+func set_title(title, color):
+	var dynamic_font = DynamicFont.new()
+	dynamic_font.font_data = load("res://Fonts/ink-free-normal.ttf")
+	dynamic_font.size = 38
+	dynamic_font.outline_color = color
+	dynamic_font.outline_size = 1
+	dynamic_font.use_filter = true
+	title.set("custom_fonts/normal_font", dynamic_font)
+	title.add_color_override("default_color", Color(color))
+func set_line_break(lineBreak, color):
+	var styleBoxLine = StyleBoxLine.new()
+	styleBoxLine.color = color
+	styleBoxLine.thickness = 4
+	lineBreak.add_stylebox_override("panel", styleBoxLine)
+	
 func _on_item_pressed(button):
 	$CanvasLayer/Book.show()
-	$CanvasLayer/Book/BookTexture/HBoxContainer/LeftContainer/MushroomTitle.bbcode_text = ItemMap[button.name].name
-	$CanvasLayer/Book/BookTexture/HBoxContainer/LeftContainer/MushroomContainer/Mushroom.texture = ItemMap[button.name].sprite
+	var title = $CanvasLayer/Book/BookTexture/HBoxContainer/LeftContainer/VBoxContainer/MushroomContainer/MushroomTitle
+	title.bbcode_text = ItemMap[button.name].name
+	set_title(title, ItemMap[button.name].color)
+	var lineBreak = $CanvasLayer/Book/BookTexture/HBoxContainer/LeftContainer/LineBreak
+	set_line_break(lineBreak, ItemMap[button.name].color)
+	$CanvasLayer/Book/BookTexture/HBoxContainer/LeftContainer/VBoxContainer/MushroomContainer/Mushroom.texture = ItemMap[button.name].sprite
 	$CanvasLayer/Book/BookTexture/HBoxContainer/LeftContainer/Description.bbcode_text = ItemMap[button.name].description
 	$CanvasLayer/Book/BookTexture/HBoxContainer/RightContainer/Notes.bbcode_text = ItemMap[button.name].notes
 	clickable_items += 1
