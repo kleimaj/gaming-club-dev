0cd1d92485cc5ffb632603ed93ce6fffe372e224
kleimaj <jacobakleiman@gmail.com>
2021-04-08 16:54:40 -0700

    animates bird flying

--- Scripts/Bird.gd
+++ Scripts/Bird.gd
@@ -1,2 +1,26 @@
 extends KinematicBody2D
 
+export var flySpeed = 250
+export var fallSpeed = 250
+var direction = 1
+var isDead = false
+var vel = Vector2()
+var anim = "flying"
+
+func _physics_process(delta):
+	vel = Vector2()
+	calc_vel()
+	move_and_collide(vel * delta)
+	$AnimatedSprite.play(anim)
+
+func calc_vel():
+	if isDead:
+		vel.y += fallSpeed
+		anim = "dead"
+		$AnimatedSprite.set_flip_v(true)
+	elif direction:
+		vel.x += flySpeed
+		$AnimatedSprite.set_flip_h(false)
+	else:
+		vel.x -= flySpeed
+		$AnimatedSprite.set_flip_h(true)
