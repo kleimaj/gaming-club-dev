03b7cfcdf631e806fb959e0848beca60e363f7e5
Gouri K <gouri.k@gmail.com>
2021-04-09 23:02:54 -0700

    Fixed the health meter animation

--- Scenes/ProgressBar.gd
+++ Scenes/ProgressBar.gd
@@ -1,7 +1,18 @@
 extends TextureProgress
 
 var totalEnemy = 8
-
+var loosePointsCount = 6
+var redLimit = 25
+var looseLimit = 15
+var maxPoints = 100
+onready var startHP = value
+onready var starthasPos =   $HealthAnim.position
+onready var currenthasPos = $HealthAnim.position
+onready var greenProgress = load("res://Assets/GFX/UI/meterInsideMiddle.png")
+onready var greenCap = load("res://Assets/GFX/UI/meterInsideCap.png")
+onready var redProgress = load("res://Assets/GFX/UI/meterInsideMiddleRed.png")
+onready var redCap = load("res://Assets/GFX/UI/meterInsideCapRed.png")
+onready var looseScene = get_parent().get_parent().get_node("CanvasLayer2/YouLose")
 
 func _on_Mushroom2_body_shape_entered(body_id, body, body_shape, area_shape):
 	value += 25
@@ -10,34 +21,52 @@ func _on_Mushroom_body_shape_entered(body_id, body, body_shape, area_shape):
 	value -= 25
 
 func incrementValue():
-	var endValue = value + ((100-value)/totalEnemy)
-	animateHealth(value, endValue)
-	#value = endValue
+	var newVal = ((maxPoints-value)/totalEnemy)
+	var endValue = value + newVal
+	var newPos = Vector2(newHealthMoveAP(endValue),currenthasPos.y)
 	totalEnemy -= 1
-	if totalEnemy == 0:
-		animateHealth(value, 100)
+	if totalEnemy <= 0:
+		$HealthAnim.visible = false
+		endValue = maxPoints
+	animateHealth(value, endValue, newPos)
 
-func decrementValue(amount=6):
-	checkHealth()
-	var endValue = value - amount
-	animateHealth(value, endValue)
-	#value = endValue
-	
-func checkHealth():
-	if value <= 25:
-		texture_progress = load("res://Assets/GFX/UI/meterInsideMiddleRed.png")
-		$FinalProgress2.texture = load("res://Assets/GFX/UI/meterInsideCapRed.png")
-		$RedAlertAP.play("RedBlink")
-	else:
-		texture_progress = load("res://Assets/GFX/UI/meterInsideMiddle.png")
-		$FinalProgress2.texture  = load("res://Assets/GFX/UI/meterInsideCap.png")
-		$RedAlertAP.stop()
-		
-	if value <= 15:
-		get_parent().get_parent().get_node("CanvasLayer2/YouLose").show()
 
-func animateHealth(startValue, endValue):
+func decrementValue(amount=loosePointsCount):
+	if totalEnemy > 0 :
+		var endValue = value - amount
+		var newPos = Vector2(newHealthMoveAP(endValue),currenthasPos.y)
+		animateHealth(value, endValue, newPos)
+			
+
+func animateHealth(startValue, endValue, animPos):
 	var animation = $HealthMoveAP.get_animation("Move")
 	animation.track_insert_key(0, 0.0, startValue)
 	animation.track_insert_key(0, 1.0, endValue)
+	animation.track_insert_key(1, 0.0, currenthasPos)
+	animation.track_insert_key(1, 1.0, animPos)
 	$HealthMoveAP.play("Move")
+	currenthasPos = animPos
+	if yield($HealthMoveAP, "animation_finished"):
+		if value <= looseLimit:
+			looseScene.show()
+		else:
+			checkHealth()
+		return
+	
+	
+func newHealthMoveAP(eVal):
+	return (eVal *  starthasPos.x)/ startHP
+	
+func checkHealth():
+	$HealthAnim.frame = 0
+	visible = true
+	if value <= redLimit:
+		$HealthAnim.play("Red")
+		texture_progress = redProgress
+		$FinalProgress2.texture = redCap
+		$RedAlertAP.play("RedBlink")
+	else:
+		$HealthAnim.play("Green")
+		texture_progress = greenProgress
+		$FinalProgress2.texture  = greenCap
+		$RedAlertAP.stop()
